d0812715ee900cb4224c3fe46bfc733350d21b76
Ratul Mistry <39340144+ratulbmg@users.noreply.github.com>
2021-09-05 00:47:18 +0530

    first commit

---
+++ C/new.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+    int a,b,c;
+    printf ("enter a number");
+    scanf ("%d",a);
+    printf ("print another number");
+    scanf ("%d",b);
+    c=a+b;
+    printf ("the addition of tow number is"c);
+    return (0);
+}
